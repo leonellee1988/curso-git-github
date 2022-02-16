62402f6651d3fc2c803693482922bde2a0687810
lee1988 <leonellee2016@gmail.com>
2022-02-16 08:44:55 -0600

    Segundo print 16.02.2022 8:44am

---
+++ config02.js
@@ -0,0 +1,6 @@
+let obtenerMensajeDos = document.getElementById("mensajeDos");
+let mostrarMensajeDos = document.getElementById("mostrarMensajeDos");
+
+obtenerMensajeDos.addEventListener("click", ()=>{
+    mostrarMensajeDos.innerHTML = "Hola mundo nuevamente, seguimos aprendiendo Git y Github";
+}); 
