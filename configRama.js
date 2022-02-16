782163c7e0ea67b65778bced4ac247c3d61fad22
lee1988 <leonellee2016@gmail.com>
2022-02-16 11:02:38 -0600

    Boton y mensaje rama 01

---
+++ configRama.js
@@ -0,0 +1,6 @@
+let obtenerMensajeTres = document.getElementById("mensajeRama");
+let mostrarMensajeTres = document.getElementById("mostrarMensajeRama");
+
+obtenerMensajeTres.addEventListener("click", ()=>{
+    mostrarMensajeTres.innerHTML = "Hola mundo nuevamente, seguimos aprendiendo Git y Github";
+}); 
